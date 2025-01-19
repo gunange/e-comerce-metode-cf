791331ccdb7f165a6f744f7c2b3d7645122e76bd
Rieyani <gunangeh7@gmail.com>
2025-01-20 07:34:29 +0900

    perbaikan urutab test

--- api/test/admin-toko/pelanggan.test.ts
+++ api/test/admin-toko/pelanggan.test.ts
@@ -8,47 +8,44 @@ const token = "173697436918a74edd26440d44b6964bd58bb9f20edc";
 let id = 1;
 
 describe("POST api/admin-toko/pelanggan", async () => {
-   
    it("should pelanggan get", async () => {
       const response = await app.request(
          "http://localhost:3000/api/admin-toko/pelanggan",
          {
             method: "GET",
-            headers :{
-               'Authorization': `Bearer ${token}`
-            }
+            headers: {
+               Authorization: `Bearer ${token}`,
+            },
          }
       );
       const body = await response.json();
-      if (debug) console.log(body);
 
       expect(response.status).toBe(200);
-   });
-   
-   it("should pelanggan add", async () => {
 
+      if (debug) console.log(body);
+   });
+
+   it("should pelanggan add", async () => {
       const response = await app.request(
          "http://localhost:3000/api/admin-toko/pelanggan",
          {
             method: "POST",
-            headers :{
-               'Authorization': `Bearer ${token}`
+            headers: {
+               Authorization: `Bearer ${token}`,
             },
-            body : JSON.stringify({
-               no_hp : "082212341234",
+            body: JSON.stringify({
+               no_hp: "082212341234",
                username: "testuser",
                nama: "testuser",
                password: "testuser",
-            })
+            }),
          }
       );
-      const body = await response.json() as any;
-      if (debug) console.log(body);
-
+      const body = (await response.json()) as any;
       id = Number(body?.data.id);
-      
 
       expect(response.status).toBe(200);
+      if (debug) console.log(body);
    });
 
    it("should pelanggan up", async () => {
@@ -56,35 +53,35 @@ describe("POST api/admin-toko/pelanggan", async () => {
          `http://localhost:3000/api/admin-toko/pelanggan/${id}`,
          {
             method: "PATCH",
-            headers :{
-               'Authorization': `Bearer ${token}`
+            headers: {
+               Authorization: `Bearer ${token}`,
             },
-            body : JSON.stringify({
-               no_hp : "082212341235",
+            body: JSON.stringify({
+               no_hp: "082212341235",
                username: "testuser",
                nama: "testuser",
                password: "testuser",
-            })
+            }),
          }
       );
       const body = await response.json();
-      if (debug) console.log(body);
 
       expect(response.status).toBe(200);
+      if (debug) console.log(body);
    });
    it("should pelanggan del", async () => {
       const response = await app.request(
          `http://localhost:3000/api/admin-toko/pelanggan/${id}`,
          {
             method: "DELETE",
-            headers :{
-               'Authorization': `Bearer ${token}`
+            headers: {
+               Authorization: `Bearer ${token}`,
             },
          }
       );
       const body = await response.json();
-      if (debug) console.log(body);
 
       expect(response.status).toBe(200);
+      if (debug) console.log(body);
    });
 });
