a8d09c0ad00d93f3a43967824ca631101eea338d
Rieyani <gunangeh7@gmail.com>
2025-02-26 19:39:13 +0900

    perbaiki to daftar - vue

--- vue/src/components/home/header/header-top.vue
+++ vue/src/components/home/header/header-top.vue
@@ -4,7 +4,7 @@
             <Button class="px-0 hover:bg-transparent text-white" label="Seller"  as="router-link" to="/daftar-seller" size="small" variant="text" severity="secondary"/>
         </div>
         <div class="flex">
-            <Button class="ms-[8px] hover:bg-transparent text-white" label="Daftar"  as="router-link" to="/login" size="small" variant="text" severity="secondary"/>
+            <Button class="ms-[8px] hover:bg-transparent text-white" label="Daftar"  as="router-link" to="/daftar" size="small" variant="text" severity="secondary"/>
             <Button class="ms-[8px] hover:bg-transparent ml-4 text-white" label="Login"  as="router-link" to="/login" size="small" variant="text" severity="secondary"/>
         </div>
     </div>
